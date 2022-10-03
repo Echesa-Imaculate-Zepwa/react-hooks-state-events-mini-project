f0d5aaf241a4b8305cce8e77ebdffe6b8fbd4ac0
Echesa Imaculate Zepwa <imaculate.zepwa@student.moringaschool.com>
2022-10-03 22:55:40 +0300

    complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,21 @@
-import React from "react";
 
-function TaskList() {
+import React from "react";
+import Task from "./Task";
+
+//Pass down the task data from `App` to `TaskList`
+function TaskList({deleteBtn, tasks}) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task,index)=>(
+        <Task 
+         category={task.category}
+          deleteBtn={deleteBtn}
+          key={index}
+          text={task.text}
+         />
+      ))}
     </div>
   );
 }
 
-export default TaskList;
+export default TaskList
